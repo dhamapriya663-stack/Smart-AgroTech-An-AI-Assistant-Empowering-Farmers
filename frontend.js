0bb48bf5fdbeef2b7bf51728e7e8a3bcc28092d3
dhamapriya663-stack <dhamapriya663@gmail.com>
2026-01-18 10:51:58 +0530

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,4 +1,20 @@
-alert("JavaScript Loaded Successfully");
+function sendOTP() {
+  document.getElementById("login").classList.add("hidden");
+  document.getElementById("otp").classList.remove("hidden");
+  alert("OTP sent to your phone (Demo OTP: 1234)");
+}
 
+function verifyOTP() {
+  const otp = document.getElementById("otpInput").value;
+  if (otp === "1234") {
+    document.getElementById("otp").classList.add("hidden");
+    document.getElementById("form").classList.remove("hidden");
+  } else {
+    alert("Invalid OTP");
+  }
+}
 
-
+function submitDetails() {
+  document.getElementById("form").classList.add("hidden");
+  document.getElementById("dashboard").classList.remove("hidden");
+}
